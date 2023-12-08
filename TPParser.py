aa8e6c57edc332a2ed67a749b634aaedd5db4ed4
Sam Dicky <samsagaze@yahoo.com>
2023-12-08 13:06:57 +0100

    découperblocs

--- TPParser.py
+++ TPParser.py
@@ -1,5 +1,28 @@
-def PremierPas(file):
-    fichier = open(file, "rb").read().hex()
+def decouperbloc(Path):
+    list=[]
+    fichier = open(Path, "rb").read().hex()
+    n=len(file)
+    i=8
+    while i<n:
+        type = fichier[i:i+2]
+        print(type)
+        if type not in [43, 48, 44]:
+            raise Exception("Problème : un bloc n'est pas H, C ou D")
+        longueur = int(fichier[i+2:i+10], 16)
+        contenu = fichier[i+10:i+10+2*longueur]
+        list.append((type, contenu))
+        i+10+2*longueur
+    if i!=n:
+        raise Exception("problème d'écarts entre la longueur annoncée et la longueur réelle")
+    return list
+
+
+
+
+
+
+def PremierPas(path):                 #file sous forme C:/...
+    fichier = open(path, "rb").read().hex()
     Largeur = int(fichier[26:34], 16)
     Hauteur = int(fichier[34:42], 16)
     Type_Pixel = int(fichier[42:44], 16)
@@ -12,13 +35,9 @@ def PremierPas(file):
     elif Type_Pixel ==3:
         Type = "couleurs 24 bits"
     else:
-        return "Error"
+        raise Exception("mauvais type de pixels")
     print("Largeur : ", Largeur)
     print("Hauteur : ", Hauteur)
     print("Type de pixel : ", Type_Pixel, " ", Type)
     return
-
-
-
-
 
