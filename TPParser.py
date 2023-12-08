86fea146a2e2627935e416670f2f98c01130003a
Sam Dicky <samsagaze@yahoo.com>
2023-12-08 12:39:56 +0100

    q1

--- TPParser.py
+++ TPParser.py
@@ -1,5 +1,24 @@
 def PremierPas(file):
-    Longueur = 0
-    Hauteur = 0
-    Type_pixel = ""
-    fichier = open(file, mode="b")
+    fichier = open(file, "rb").read().hex()
+    Largeur = int(fichier[26:34], 16)
+    Hauteur = int(fichier[34:42], 16)
+    Type_Pixel = int(fichier[42:44], 16)
+    if Type_Pixel == 0:
+        Type = "noir et blanc"
+    elif Type_Pixel == 1:
+        Type = "niveaux de gris"
+    elif Type_Pixel ==2:
+        Type = "palette"
+    elif Type_Pixel ==3:
+        Type = "couleurs 24 bits"
+    else:
+        return "Error"
+    print("Largeur : ", Largeur)
+    print("Hauteur : ", Hauteur)
+    print("Type de pixel : ", Type_Pixel, " ", Type)
+    return
+
+
+
+
+
--- test.py
+++ test.py
@@ -3,4 +3,11 @@ def test(file):
     return open(file, 'rb')
 
 path = "C:/Users/samsa/Downloads/minipng-samples/minipng-samples/bw/ok/A.mp"
-print(binascii.hexlify(test(path).read()))
+file=binascii.hexlify(test(path).read())
+file0=test(path).read().hex()
+#print(test(path).read())
+#rint(test(path).read()[0])
+
+def t():
+    return "a", "b"
+print(t())
